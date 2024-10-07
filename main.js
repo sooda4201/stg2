0b50a74b74dd3621688fb25109b1daafa88eda4a
sooda <shin.et0613@gmail.com>
2024-10-07 11:59:40 +0900

    a

---
+++ main.js
@@ -0,0 +1,23 @@
+document.onkeydown=function(e)
+{
+    switch(e.keycode)
+    {
+        case 32:
+        
+        //フィールドサイズ
+        const FIELD_W=200;
+        const FIELD_H=100;
+
+        //スクリーンサイズ
+        const SCREEN_W=FIELD_W*2;
+        const SCREEN_H=FIELD_H*2;
+
+        //キャンバスサイズ
+        const CANVAS_W=SCREEN_W;
+        const CANVAS_H=SCREEN_H;
+
+        //
+
+
+    }
+}
