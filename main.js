8275a31c3a0d30fd22ef4e777b69b362afa0b72b
sooda <shin.et0613@gmail.com>
2024-10-15 21:08:53 +0900

    a

--- main.js
+++ main.js
@@ -61,6 +61,6 @@ class Star
 let star=[];
 for(let i=0;1<STAR_MAX;i++)star[i]= new Star();
 
-con.fillStyle="black";
-con.fillRect(0,0,SCREEN_W,SCREEN_H);
+//con.fillStyle="black";
+//con.fillRect(0,0,SCREEN_W,SCREEN_H);
 for(let i=0;1<STAR_MAX;i++)star[i].draw();
