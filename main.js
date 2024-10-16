ba3f18cfef31f5c98d24e2cb2c6cdbfa916278c5
sooda <shin.et0613@gmail.com>
2024-10-16 18:52:43 +0900

    q

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 //画面サイズ
-const SCREEN_W=180;
-const SCREEN_H=320;
+const SCREEN_W = 180; 
+const SCREEN_H = 320;
 
 //キャンバスサイズ
-const CANVAS_W=SCREEN_W*2;
-const CANVAS_H=SCREEN_H*2;
+const CANVAS_W = SCREEN_W *2;
+const CANVAS_H = SCREEN_H *2;
 
 //フィールドサイズ
-const FIELD_W=SCREEN_W*2;
-const FIELD_H=SCREEN_H*2;
+const FIELD_W = SCREEN_W *2;
+const FIELD_H = SCREEN_H *2;
 
-//星の数
-const STAR_MAX=300;
+// 星の数
+const STAR_MAX = 300;
 
-//キャンバス
-let can=decument.getElementById("can");
-let con=can.getContext("2d");
-can.width=CANVAS_W;
-can.height=CANVAS_H;
+//星の実体
+let star = [];
+for (let i=0; i<STAR_MAX ; i++) star[i] = new star();
 
-//整数の乱数
-function rand(min,max)
+
+// キャンバス
+let can = document.getElementById("can");
+let con = can.getContext("2d");
+can.width = CANVAS_W;
+can.height = CANVAS_H;
+
+//整数の乱数を取得
+function rand(min, max) 
 {
-    return Math.floor(Math.random()*(max-min+1))+min;
+    return Math.floor(Math.random()*(max-min+1))+ min;
 }
 
 //星クラス
@@ -30,38 +35,37 @@ class Star
 {
     constructor()
     {
-        this.x=rand(0,FIELD_W)<<8;
-        this.y=rand(0,FIELD_H)<<8;
-        this.vx=0;
-        this.vy=rand(30,500);
-        this.sz=rand(1,2);
+        this.x  = rand(0, FIELD_W)<<8; //ビット演算子
+        this.y = rand(0, FIELD_H)<<8;
+        this.vx = 0;
+        this.vy = rand(30, 500);
+        this.sz = rand(1, 2)
     }
-
     draw()
     {
         let x=this.x>>8;
         let y=this.y>>8;
 
-        vcon.fillStyle=(rand(0,2)!=0)?"#66f":"#8af";
-        vcon.fillRect(x,y,this.sz,this.sz);
+        vcon.fillStyle = (rand(0, 2) != 0)?"#66f" : "#8af" ;
+        vcon.fillRect(x, y, this.sz,this.sz);
+
     }
 
     update()
     {
         this.x += this.vx;
         this.y += this.vy;
-        if(this.y>FIELD_H<<8)
+        
+        if (this.y > FIELD_H << 8) 
         {
-            this.y=0;
-            this.x=rand(0,FIELD_W)<<8;
+            this.y = 0;
+            this.x = rand(0, FIELD_W) << 8;
         }
     }
 }
 
-//星の実体
-let star=[];
-for(let i=0;i<STAR_MAX;i++)star[i]=new Star();
 
-vcon.fillStyle="black";
-vcon.fillRect(0,0,SCREEN_W,SCREEN_H);
-for(let i=0;i<STAR_MAX;i++)star[i].draw();
+vcon.fillStyle = "black";
+vcon.fillRect(0, 0, SCREEN_W, SCREEN_H);
+
+for (let i=0; i<STAR_MAX ; i++) star[i] .draw();
